3c839cd37b7419b5e7829840db2a632828b5722f
Justin <justin.byrne65@gmail.com>
2022-10-25 05:42:46 +1100

    Made sword swing

---
+++ Assets/Scripts/PuzzleDoor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDoor : MonoBehaviour
+{
+    public void Open() {
+        // TODO: Animate pls
+        gameObject.SetActive(false);
+    }
+}
